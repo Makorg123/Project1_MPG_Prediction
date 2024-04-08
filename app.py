a0d88a17b595363d6619a14c9778c406499a3a63
Makorg123 <132anaskhan@gmail.com>
2024-04-08 14:25:25 +0530

    Update car features sliders in app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ st.markdown('This app predicts the **MPG** of a car!')
 st.header('Car Features')
 col1,col2,col3,col4 = st.columns(4)
 with col1:
-    cylinders = st.slider('cylinders',2,8,1)
-    displacement = st.slider('displacement',50,500,10)
+    cylinders = st.slider('cylinders',3,8,3)
+    displacement = st.slider('displacement',68,455,10)
 with col2:
-      horsepower = st.slider('horsepower',50,500,10)
-      weight = st.slider('weight',1500,6000,250)
+      horsepower = st.slider('horsepower',46,230,10)
+      weight = st.slider('weight',1500,5200,100)
 with col3:
       acceleration = st.slider('acceleration',8,25,1)
       model_year = st.slider('model_year',70,85,70)
